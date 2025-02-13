da7be0a97462c08665a88718c98aa1667cde75b8
Marwan du Bois <562196@student.fontys.nl>
2025-02-13 13:35:50 +0100

    Added welcome

--- Gatekeeper.go
+++ Gatekeeper.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Welkom bij Fonteyn Vakantieparken")
+	hour := time.Now().Hour()
+
+	if hour >= 23 || hour < 7 {
+		fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten")
+	} else if hour < 12 {
+		fmt.Println("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+	} else if hour < 18 {
+		fmt.Println("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+	} else {
+		fmt.Println("Goedenavond! Welkom bij Fonteyn Vakantieparken")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Gatekeeper.go
+
+go 1.24.0
